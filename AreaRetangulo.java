2b83b028b0637e98e7f7e85357c2bb181a5a89de
Maria Clara Ramalho Medeiros <110572765+marysclair@users.noreply.github.com>
2023-03-27 15:12:37 -0300

    Update and rename AreaRetangulo.java to CBBAtividadePratica3.java

--- AreaRetangulo.java
+++
@@ -1,8 +0,0 @@
-public class AreaRetangulo{
-    public static void main(String[] args){
-        int base = 10;
-        int altura = 5;
-        float area = (base*altura)/2;
-        System.out.print("A área do retângulo de base " + base + "(cm) e altura " + altura + "(cm) é: " + area + "(cm2)");
-    }
-}
